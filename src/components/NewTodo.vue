333da93e07eee2880bbac293fdae3a100dc8fab3
SmartDev <dev10dragon10@gmail.com>
2023-01-26 16:17:28 -0600

    UI step1 finished (header & newItem)

---
+++ src/components/NewTodo.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <input type="text" class="bg-transparent border border-solid border-white rounded-md px-5 py-2 text-white text-lg mr-10" placeholder="New task" />
+        <button class="bg-transparent border border-solid rounded-md border-white px-5 py-2 text-lg text-white transition-all duration-100 hover:px-10">Add</button>
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,20 @@
 <script setup>
 import TodoHeader from './TodoHeader.vue';
+import NewTodo from './NewTodo.vue';
+import TodoList from './TodoList.vue';
+import { ref, onMounted } from 'vue'
+
+const todos = ref([]);
+
 </script>
 
 <template>
     <div class="md:container md:mx-auto">
         <TodoHeader />
+        <div class="flex flex-col items-center">
+            <NewTodo />
+            <TodoList todos />
+        </div>
     </div>
 </template>
 
